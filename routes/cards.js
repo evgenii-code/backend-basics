82d95d1884dd9147d9b7bfe7187cee59475a3d56
evgenii-code <osipenkov.evgenii@gmail.com>
2020-07-22 13:27:00 +0300

    Read file middleware

--- routes/cards.js
+++ routes/cards.js
@@ -1,12 +1,28 @@
 const router = require('express').Router();
 const path = require('path');
+const fs = require('fs');
+const { isJsonString, rootPath } = require('../utils');
 
-const cards = require(path.join(path.dirname(require.main.filename), '/data/cards.json'));
+const filePath = path.join(rootPath, '/data/cards.json');
+let cards;
+
+const doesFileExist = (req, res, next) => {
+  fs.readFile(filePath, { encoding: 'utf8' }, (err, data) => {
+    if (err || !isJsonString(data)) {
+      res.status(500).send({ message: err || `Ошибка чтения файла ${filePath}` });
+      return;
+    }
+
+    cards = JSON.parse(data);
+    next();
+  });
+};
 
 const sendCards = (req, res) => {
   res.send(cards);
 };
 
+router.get('/cards', doesFileExist);
 router.get('/cards', sendCards);
 
 module.exports = router;

309486ff50f30ee123c2b8ee6fd9a518fa6db03d
evgenii-code <osipenkov.evgenii@gmail.com>
2020-08-07 15:25:17 +0300

    Models created

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const { linkValidator } = require('../utils');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+    required: true,
+    validate: linkValidator,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);

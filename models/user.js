4cc58bd86cd676d1e17e77e0120ac258aa986405
evgenii-code <osipenkov.evgenii@gmail.com>
2020-08-25 17:15:57 +0300

    Email & password added to the Schema

--- models/user.js
+++ models/user.js
@@ -23,6 +23,21 @@ const userSchema = new mongoose.Schema({
       },
     },
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(email) {
+        return validator.isEmail(email);
+      },
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
 });
 
 module.exports = mongoose.model('user', userSchema);

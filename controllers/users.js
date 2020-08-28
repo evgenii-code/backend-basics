5f2f4902559eccbf301855fd22ee1edff1c7c65a
evgenii-code <osipenkov.evgenii@gmail.com>
2020-08-28 15:33:44 +0300

    Default values without .env

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const { defineErrorCode, defineErrorMessage } = require('../utils/utils');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
+const { NODE_ENV = 'development', JWT_SECRET } = process.env;
 
 module.exports.getUsers = (req, res) => {
   User.find({})
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
+const { NODE_ENV = 'development', JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;

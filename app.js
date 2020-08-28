6b77522e3e2ad1a5a5b07877a71c1666b0340e21
evgenii-code <osipenkov.evgenii@gmail.com>
2020-08-28 15:32:23 +0300

    Removed static path

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const express = require('express');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
@@ -28,7 +27,6 @@ app.use(limiter);
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/signin', login);
 app.post('/signup', createUser);

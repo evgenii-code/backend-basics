dfb742652acf5b2c7ea7271b64e2949aff065a6b
evgenii-code <osipenkov.evgenii@gmail.com>
2020-09-17 20:12:16 +0300

    Validation fix

--- routes/cards.js
+++ routes/cards.js
@@ -4,10 +4,16 @@ const { upload } = require('../utils/multer-config');
 const optimize = require('../middlewares/optimize');
 const {
   // createCardScheme,
+  createCardFromFileScheme,
   cardIdScheme,
 } = require('../utils/validation-schemes');
 const {
-  getCards, createCard, deleteCard, likeCard, dislikeCard,
+  getCards,
+  // createCard,
+  createCardFromFile,
+  deleteCard,
+  likeCard,
+  dislikeCard,
 } = require('../controllers/cards');
 // const { authSchema } = require('../utils/validation-schemes'); // no joi validation for cookie
 const auth = require('../middlewares/auth');
@@ -16,7 +22,7 @@ router.get('/', getCards);
 // router.use(celebrate(authSchema), auth); // no joi validation for cookie
 router.use(auth);
 // router.post('/', celebrate(createCardScheme), createCard);
-router.post('/', upload.single('picture'), optimize, createCard);
+router.post('/', upload.single('file'), celebrate(createCardFromFileScheme), optimize, createCardFromFile);
 router.delete('/:cardId', celebrate(cardIdScheme), deleteCard);
 router.put('/:cardId/likes', celebrate(cardIdScheme), likeCard);
 router.delete('/:cardId/likes', celebrate(cardIdScheme), dislikeCard);
--- utils/validation-schemes.js
+++ utils/validation-schemes.js
@@ -52,6 +52,12 @@ module.exports.createCardScheme = {
   }),
 };
 
+module.exports.createCardFromFileScheme = {
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+  }),
+};
+
 module.exports.cardIdScheme = {
   params: Joi.object().keys({
     cardId: Joi.string().hex().length(24),

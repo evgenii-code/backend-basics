f1cd10e25583562df371379427b6e99e217452e5
evgenii-code <osipenkov.evgenii@gmail.com>
2020-09-17 18:45:58 +0300

    Deleted console.log

--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { celebrate } = require('celebrate');
 const { upload } = require('../utils/multer-config');
+const optimize = require('../middlewares/optimize');
 const {
   // createCardScheme,
   cardIdScheme,
@@ -15,7 +16,7 @@ router.get('/', getCards);
 // router.use(celebrate(authSchema), auth); // no joi validation for cookie
 router.use(auth);
 // router.post('/', celebrate(createCardScheme), createCard);
-router.post('/', upload.single('picture'), createCard);
+router.post('/', upload.single('picture'), optimize, createCard);
 router.delete('/:cardId', celebrate(cardIdScheme), deleteCard);
 router.put('/:cardId/likes', celebrate(cardIdScheme), likeCard);
 router.delete('/:cardId/likes', celebrate(cardIdScheme), dislikeCard);

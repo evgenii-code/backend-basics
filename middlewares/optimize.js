5e2cfb8449c6f8bc7abab7e6bc3144bc5d2d8fef
evgenii-code <osipenkov.evgenii@gmail.com>
2020-09-18 13:46:01 +0300

    Catch block in optimize middleware

--- middlewares/optimize.js
+++ middlewares/optimize.js
@@ -1,11 +1,13 @@
 const tinify = require('tinify');
+const ServiceUnavailable = require('../errors/service-unavailable');
 require('dotenv').config();
 
 tinify.key = process.env.TINIFY_KEY;
 
 module.exports = (req, res, next) => {
   const source = tinify.fromFile(req.file.path);
-  source.toFile(req.file.path);
 
-  return next();
+  source.toFile(req.file.path)
+    .then(() => next())
+    .catch(() => next(new ServiceUnavailable('Ошибка загрузки файла на Tinify')));
 };
